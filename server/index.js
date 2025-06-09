1681225d4123d342c68e61e2b8e0e10dedde4cf1
Oleg Golub <oleggolub46@gmail.com>
2025-06-09 20:24:38 +0300

    v3

--- server/index.js
+++ server/index.js
@@ -3,19 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require('./routes/api/children');
-
 app.use('/api/posts', posts);
 
 mongoose.connect(process.env.URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const port = process.env.PORT || 5000;
 
+
+const __dirnameBase = path.resolve();
+app.use(express.static(path.join(__dirnameBase, 'client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirnameBase, 'client/dist', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
